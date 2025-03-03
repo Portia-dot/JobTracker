608b05584b7cd0cccf1b5d4542d933d5e5000f10
“Portia-Dot <modamorio21@mytru.ca>
2025-03-03 05:52:53 -0800

    Updated job scraper and database integration

--- database.py
+++ database.py
@@ -2,29 +2,37 @@ import sqlite3
 
 class Database:
     def __init__(self, dbName="jobs.db"):
-        self.conn = sqlite3.connect('dbName')
-        selfcursor = self.conn.cursor()
-        self.create
+        self.conn = sqlite3.connect('jobs.db')
+        self.cursor = self.conn.cursor()
+        self.createTables()
 
 
-#Create a table
+    #Create a table
 
-def createTables(self):
-    self.cursor.execute("""
-    CREATE TABLE IF NOT EXISTS jobs ( id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title TEXT, company TEXT,location TEXT, timePosted TEXT,
-    salary TEXT, jobType TEXT
-    )
-    """)
-    self.conn.commit()
+    def createTables(self):
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS jobs ( id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+         title TEXT,
+          companyName TEXT,
+          location TEXT,
+           timePosted TEXT,
+           salary TEXT,
+            jobType TEXT
+        )
+        """)
+        self.conn.commit()
 
 
 
 
-def insertJob(self, job):
-    self.cursor.execute("""INSERT INTO jobs (title, company, location, timePosted, salary, jobType) VALUES (?,?,?,?,?)""",
-                   (job["title"], job["company"], job["location"], job["timePosted"], job["salary"], job["jobType"]))
-    self.conn.commit()
+    def insertJob(self, job):
+        self.cursor.execute("""INSERT INTO jobs (title, companyName, location, timePosted, salary, jobType) VALUES (?,?,?,?,?,?)""",
+                       (job["title"], job["companyName"], job["location"], job["timePosted"], job["salary"], job["jobType"]))
+        self.conn.commit()
 
-def fetchJobs(self):
-    self.cursor.execute("""SELECT * FROM jobs""")
-    return self.cursor.fetchall()
+    def fetchJobs(self):
+        self.cursor.execute("""SELECT * FROM jobs""")
+        return self.cursor.fetchall()
+
+    def close(self):
+        self.conn.close()
--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from scraper import Scraper
+from database import Database
 if __name__ == "__main__":
     scraper = Scraper()
-    jobs = scraper.searchGoogleJobs("Data Scientist", 'Canada', maxResults = 100, timeFilter = "d")
+    database = Database()
+
+    jobs = scraper.searchGoogleJobs("Data Scientist", 'Canada', maxResults = 10, timeFilter = "d")
+
+    #Save Each Job
+    for job in jobs:
+        database.insertJob(job)
+
+
+    #Fetch and Print Jobs
+    storeJob = database.fetchJobs()
+    for job in storeJob:
+        print(job)
+#Close browser and Database
+    database.close()
     scraper.closeDriver()
 
